72e75e36ddf473ef906664df4834bdf5da57c31f
Rindula <rindola@t-online.de>
2017-09-19 18:32:02 +0200

    Kommentiererung hinzugefügt

--- Seite 1/pageManager.js
+++ Seite 1/pageManager.js
@@ -1,10 +1,31 @@
 function page(id)
 {
 
+    /*
+    Wenn die Funktion aufgerufen wird, wird das Parameter "id" genutzt, um die Seite zu wechseln.
+    Beispiel:
+    
+    page(1)
+    springt zur nächsten seite
+    
+    page(2)
+    springt zwei seiten weiter
+
+    page(-1)
+    springt eine seite zurück
+
+    Parameter:
+    id  - Integer
+    */
+
+    // Verrechnung von der id -> pId (Maximal 5, minimal 1)
     pId = Math.max(Math.min(5,pId + id), 1);
 
+    // Ändern der Bild Quelle
     document.getElementById("img").src = "img/igel" + pId + ".jpg";
 
 }
+
+// Setzen der ID für das Bild
 
 pId = 1;
